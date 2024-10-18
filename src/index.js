dc1f5289b5d9fbf6709f15beadb5afea0210ac36
Sheldon <sheldonc@iastate.edu>
2024-10-18 15:50:40 -0500

    created react

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<p>Hi</p>);
